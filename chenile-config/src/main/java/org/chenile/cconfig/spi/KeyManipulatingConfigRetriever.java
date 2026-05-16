e78b100cbdaa8fb19e8628e1c2dbbd2a412d81b5
Raja Shankar Kolluru <raja@thekollurus.com>
2026-05-16 23:41:28 +0530

    Upgrade to 2.1.21

--- chenile-config/src/main/java/org/chenile/cconfig/spi/KeyManipulatingConfigRetriever.java
+++ chenile-config/src/main/java/org/chenile/cconfig/spi/KeyManipulatingConfigRetriever.java
@@ -6,6 +6,13 @@ import org.chenile.owiz.Command;
 
 import java.util.List;
 
+/**
+ * This needs to be enhanced by all the config retrievers who want to allow manipulating existing keys.
+ * They need to return a bunch of CConfigs - each with the intent if manipulating one key (there can be
+ * multiple CConfigs that manipulate the same key).
+ * <p>The CConfig can replace an entire key or parts of it as specified in the {@link Cconfig#path}</p>
+ * The expression language supported is SPEL. Others can also be used.
+ */
 public abstract class KeyManipulatingConfigRetriever implements Command<ConfigContext> {
     @Override
     public final void execute(ConfigContext configContext) {
